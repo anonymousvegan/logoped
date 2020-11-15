bd9380edf0ef817e5d3a65bf29746743637d4253
nikola <majjunjulavgust@gmail.com>
2020-11-15 14:39:24 +0100

    3th page finished

--- footer/footer.php
+++ footer/footer.php
@@ -8,6 +8,6 @@ $date=date("Y");
 <div id="social-icons">
     <a href="https://www.linkedin.com/in/nermina-numanovi%C4%87-350435155/"><img src="files/linkedin.png" ></a>
     <a href="https://www.facebook.com/%D0%9E%D0%BA%D1%83%D0%BF%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%B0-%D1%82%D0%B5%D1%80%D0%B0%D0%BF%D0%B8%D1%98%D0%B0-%D0%B8-%D0%BB%D0%BE%D0%B3%D0%BE%D0%BF%D0%B5%D0%B4%D0%B8%D1%98%D0%B0-105079674525215"><img src="files/facebook.png" ></a>
-    <a href="#"><img src="files/instagram.png" ></a>
+    <a href="https://www.instagram.com/okupacionaterapija_logopedija/"><img src="files/instagram.png" ></a>
 </div>
 </footer>
--- script/set-active.js
+++ script/set-active.js
@@ -1,8 +1,15 @@
+var number;
 function setActive(n){
+    number=n;
     const nav = document.getElementById("horisontal-nav");
     const activeNav = nav.querySelectorAll(".nav-link")[n];
     activeNav.classList.add("active")
     const verticalNav = document.getElementById("vertical-nav");
     const verticalActiveNav = verticalNav.querySelectorAll(".nav-link")[n];
     verticalActiveNav.classList.add("active")
+}
+function setDropActive(a){
+    document.getElementById("horisontal-nav")
+    .querySelectorAll(".nav-link")[number]
+    .querySelectorAll("a")[a].classList.add("active");
 }
